bf7b9a3f5128f43febd5e64f76e8dabd9feafd7a
ret2ex <kosongisnotzero@gmail.com>
2019-11-14 19:07:47 +0700

    First Commit

---
+++ application/models/M_siswa.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_siswa extends CI_Model {
+
+public function get_siswakelas($id_kelas)
+	{
+		return $this->db->query("SELECT * from tblsiswa where id_kelas='$id_kelas' order by nama_siswa ASC");
+	}
+public function get_jumlah_siswa($id_kelas)
+	{
+		return $this->db->query("SELECT count(*) as jumlah from tblsiswa where id_kelas='$id_kelas'");
+	}
+}
+?>
